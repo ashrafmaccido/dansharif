d86c6b702d510e1de1bc12bca7e72fc8619f466a
ashrafmaccido <ashrafmaccido@gmail.com>
2024-03-28 14:13:11 +0100

    added notifications api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('remita-payment-notifications', function(Request $request) {
+    return response()->json([
+        'status' => 200,
+        'statusText' => 'OK',
+    ], 200);
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
